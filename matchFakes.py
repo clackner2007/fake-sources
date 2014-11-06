c6dcc781e028065923a43cf0728d5256ccc7e137
Claire Lackner <clairelackner@gmail.com>
2014-11-06 13:28:27 +0900

    updated matchFakes to create new table with additional field for fakeid

--- matchFakes.py
+++ matchFakes.py
@@ -5,13 +5,13 @@ matches fakes based on position stored in the calibrated exposure image header
 """
 
 import lsst.daf.persistence as dafPersist
-from lsst.afw.table.tableLib import SourceCatalog
+from lsst.afw.table import SourceCatalog, SchemaMapper
 import numpy as np
 import argparse
 import re
 import collections
 
-def getFakeSources(rootdir, dataId, tol=0.1):
+def getFakeSources(rootdir, dataId, tol=1.0):
     """Get list of sources which agree in position with fake ones with tol
     """
     butler = dafPersist.Butler(rootdir)
@@ -37,12 +37,18 @@ def getFakeSources(rootdir, dataId, tol=0.1):
         s1 = sources.subset(matched)
         srcIndex[fid] = np.where(matched)[0]
 
-    srcList = None
-    for s in srcIndex.values():
-        for ss in s:
-            if srcList is None:
-                srcList = SourceCatalog(sources.getSchema())
-            srcList.append(sources[ss])
+    mapper = SchemaMapper(sources.schema)
+    mapper.addMinimalSchema(sources.schema)
+    newSchema = mapper.getOutputSchema()
+    newSchema.addField('fakeId', type=int, doc='id of fake source matched to position')
+    srcList = SourceCatalog(newSchema)
+    srcList.reserve(sum([len(s) for s in srcIndex.values()]))
+
+    for ident, sindlist in srcIndex.items():
+        for ss in sindlist:
+            newRec = srcList.addNew()
+            newRec.assign(sources[ss], mapper)
+            newRec.set('fakeId', ident)
     return srcIndex, srcList
 
 
@@ -55,8 +61,18 @@ def main():
     parser.add_argument('ccd', help='id of ccd', type=int)
     args = parser.parse_args()
     
-    print getFakeSources(args.rootDir, {'visit':args.visit, 'ccd':args.ccd})
+    sind, slist = getFakeSources(args.rootDir, 
+                                 {'visit':args.visit, 'ccd':args.ccd})
 
+    butler = dafPersist.Butler(args.rootDir)
+    md = butler.get('calexp_md', {'visit':args.visit,
+                                  'ccd':args.ccd})
+    zp = 2.5 * np.log10(md.get('FLUXMAG0'))
+    print 'fakeId X Y cmodel_mag nChild kron_mag'
+    for s in slist:
+        mag = lambda name:-2.5 * np.log10(s.get(name)) + zp
+        print s.get('fakeId'), s.get('centroid.sdss')[0], s.get('centroid.sdss')[1], mag('cmodel.flux'), 
+        print s.get('deblend.nchild'), mag('flux.kron')
 
     
 if __name__=='__main__':
