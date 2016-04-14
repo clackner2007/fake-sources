60267da65e20c981a3f7b681495d0979be3c584b
dr.guangtou <dr.guangtou@gmail.com>
2016-04-14 19:11:43 +0900

    Try..

--- python/fakes/addFakes.py
+++ python/fakes/addFakes.py
@@ -152,6 +152,7 @@ class addFakesTask(BatchPoolTask):
                 self.log.info("Removing unused mask plane")
                 exposure.getMaskedImage().getMask().removeMaskPlane('CR')
                 exposure.getMaskedImage().getMask().removeMaskPlane('SAT')
+                exposure.getMaskedImage().getMask().removeMaskPlane('SUSPECT')
                 exposure.getMaskedImage().getMask().removeMaskPlane('CROSSTALK')
                 exposure.getMaskedImage().getMask().removeMaskPlane('UNMASKEDNAN')
                 dataRef.put(exposure, "calexp")
