55bf7736a9d6aabe8ca862738f29695c31810f96
dr.guangtou <dr.guangtou@gmail.com>
2016-06-30 02:11:18 +0900

    Clean the code

--- python/fakes/makeRaDecCat.py
+++ python/fakes/makeRaDecCat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 """
 Create N random selected (RA, Dec) pairs within a desired region.
 The inputs can be:
@@ -6,26 +7,30 @@ The inputs can be:
    2. DataId for single frame image: (visit, ccd)
    3. DataId for coadd image: (tract, patch, filter)
 """
+
 import os
+import warnings
+
 import numpy as np
 from numpy.random import uniform
 
 
 def polyReadWkb(wkbName, load=True):
+    from shapely.wkt import loads
 
     wkbFile = open(wkbName, 'r')
     polyWkb = wkbFile.read().decode('hex')
     wkbFile.close()
 
     if load is True:
-        return wkb.loads(polyWkb)
+        return loads(polyWkb)
     else:
         return polyWkb
 
 
 def getImageRaDecRange(rootDir, dataId, dataType='calexp'):
     """
-    Get the Ra,Dec range for certain single frame or coadded image using the WCS
+    Get the Ra,Dec range for certain single frame or coadded image
     """
 
     import lsst.daf.persistence as dafPersist
@@ -35,17 +40,17 @@ def getImageRaDecRange(rootDir, dataId, dataType='calexp'):
     exposure = butler.get(dataType, dataId)
 
     bboxI = exposure.getBBox(afwImage.PARENT)
-    wcs   = exposure.getWcs()
+    wcs = exposure.getWcs()
 
     minPix = wcs.pixelToSky(bboxI.getMinX(), bboxI.getMinY())
     maxPix = wcs.pixelToSky(bboxI.getMaxX(), bboxI.getMaxY())
 
-    ra1  = minPix.getLongitude().asDegrees()
-    ra2  = maxPix.getLongitude().asDegrees()
+    ra1 = minPix.getLongitude().asDegrees()
+    ra2 = maxPix.getLongitude().asDegrees()
     dec1 = minPix.getLatitude().asDegrees()
     dec2 = maxPix.getLatitude().asDegrees()
 
-    minRa, maxRa   = min(ra1, ra2), max(ra1, ra2)
+    minRa, maxRa = min(ra1, ra2), max(ra1, ra2)
     minDec, maxDec = min(dec1, dec2), max(dec1, dec2)
 
     return [minRa, maxRa, minDec, maxDec]
@@ -59,15 +64,15 @@ def getRandomRaDec(nRand, minRa, maxRa, minDec, maxDec, rad=None):
         raise Exception('Please provide appropriate Ra,Dec range !')
 
     if rad is None:
-        raArr  = uniform(low=minRa,  high=maxRa,  size=nRand)
+        raArr = uniform(low=minRa,  high=maxRa,  size=nRand)
         decArr = uniform(low=minDec, high=maxDec, size=nRand)
         return zip(raArr, decArr)
     else:
         import lsst.afw.coord as afwCoord
-        import lsst.afw.geom  as afwGeom
+        import lsst.afw.geom as afwGeom
 
         minSep = float(rad)
-        raArr  = []
+        raArr = []
         decArr = []
         numTry = 0
         while len(raArr) < nRand:
@@ -76,7 +81,7 @@ def getRandomRaDec(nRand, minRa, maxRa, minDec, maxDec, rad=None):
                 decArr.append(uniform(low=minDec, high=maxDec))
                 numTry += 1
             else:
-                raTry  = uniform(low=minRa,  high=maxRa)
+                raTry = uniform(low=minRa,  high=maxRa)
                 decTry = uniform(low=minDec, high=maxDec)
                 coordTry = afwCoord.Coord(afwGeom.Point2D(raTry, decTry))
                 nExist = len(raArr)
@@ -86,8 +91,6 @@ def getRandomRaDec(nRand, minRa, maxRa, minDec, maxDec, rad=None):
                                                                decArr[ii]))
                     sep = coordTry.angularSeparation(coordTest).asArcseconds()
                     if sep <= minSep:
-                        print "## BAD ONE %8d : %8d -- %f10 <= %f10 !!" % (len(raArr),
-                                                                     ii, sep, minSep)
                         sepGood = False
                         break
                 if sepGood:
@@ -106,20 +109,24 @@ def plotRandomRaDec(randomRaDec, rangeRaDec=None):
     plt.xlabel(r'RA (J2000)',  fontsize=20, labelpad=20)
     plt.ylabel(r'DEC (J2000)', fontsize=20, labelpad=20)
 
+    # TODO : To be finished
+    """
     if rangeRaDec is not None:
         if type(rangeRaDec) is dict:
-            raMin,  raMax  = rangeRaDec['raMin'], rangeRaDec['raMax']
+            raMin, raMax = rangeRaDec['raMin'], rangeRaDec['raMax']
             decMin, decMax = rangeRaDec['raMin'], rangeRaDec['raMax']
         else:
-            raMin,  raMax  = rangeRaDec[0], rangeRaDec[1]
+            raMin, raMax = rangeRaDec[0], rangeRaDec[1]
             decMin, decMax = rangeRaDec[0], rangeRaDec[1]
-        raDec0   = (raMin, decMin)
-        raRange  = (raMax  - raMin)
+        raDec0 = (raMin, decMin)
+        raRange = (raMax - raMin)
         decRange = (decMax - decMin)
+    """
 
     plt.gcf().savefig('randomRaDec.png')
 
     return None
+
 
 def makeRaDecCat(nRand, dataId=None, rangeRaDec=None, rad=None,
                  rootDir='/lustre/Subaru/SSP/rerun/yasuda/SSP3.8.5_20150725/',
@@ -135,30 +142,28 @@ def makeRaDecCat(nRand, dataId=None, rangeRaDec=None, rad=None,
     if dataId is not None:
         if 'visit' in dataId.keys() and 'ccd' in dataId.keys():
             # Input should be a single frame image
-            rangeRaDec  = getImageRaDecRange(rootDir, dataId)
+            rangeRaDec = getImageRaDecRange(rootDir, dataId)
             randomRaDec = getRandomRaDec(nRand, rangeRaDec[0], rangeRaDec[1],
                                          rangeRaDec[2], rangeRaDec[3], rad=rad)
-        elif 'tract' in dataId.keys() and 'patch' in dataId.keys() and 'filter' in dataId.keys:
+        elif ('tract' in dataId.keys()
+              and 'patch' in dataId.keys()
+              and 'filter' in dataId.keys):
             # Input should be a coadd image
-            rangeRaDec  = getImageRaDecRange(rootDir, dataId,
+            rangeRaDec = getImageRaDecRange(rootDir, dataId,
                                             dataType='deepCoadd_calexp')
             randomRaDec = getRandomRaDec(nRand, rangeRaDec[0], rangeRaDec[1],
                                          rangeRaDec[2], rangeRaDec[3], rad=rad)
         else:
             raise KeyError('Please provide the correct dataId !')
-
     elif rangeRaDec is not None:
-
         if type(rangeRaDec) is dict:
-            rKeys = rangeRaDec.keys()
-            #if 'raMin' in rKeys and 'raMax' in rKeys and 'decMin' in rKeys and 'decMax' in rKeys:
+            # rKeys = rangeRaDec.keys()
             randomRaDec = getRandomRaDec(nRand, rangeRaDec['minRa'],
                                          rangeRaDec['maxRa'],
                                          rangeRaDec['minDec'],
                                          rangeRaDec['maxDec'], rad=rad)
-            #else:
-            #    raise KeyError('Please provide the correct rangeRaDec!')
-        elif type(rangeRaDec) is list or type(rangeRaDec).__module__ == 'numpy':
+        elif (type(rangeRaDec) is list
+              or type(rangeRaDec).__module__ == 'numpy'):
             if len(rangeRaDec) >= 4:
                 randomRaDec = getRandomRaDec(nRand, rangeRaDec[0],
                                              rangeRaDec[1], rangeRaDec[2],
@@ -167,7 +172,6 @@ def makeRaDecCat(nRand, dataId=None, rangeRaDec=None, rad=None,
                 raise Exception('randomRaDec should have at least 4 elements!')
         else:
             raise Exception('randomRaDec need to be Dict/List/Numpy.Array')
-
     else:
         raise Exception("Need to provide either dataId or rangeRaDec")
 
@@ -177,27 +181,29 @@ def makeRaDecCat(nRand, dataId=None, rangeRaDec=None, rad=None,
     """
     if acpMask is not None or rejMask is not None:
         try:
-            from shapely import wkb
-            from shapely.geometry import Polygon, LineString, Point
+            from shapely.geometry import Point
             from shapely.prepared import prep
-        except ImportError:
-            raise Exception('Please install the Shapely library before using this function')
-        raArr, decArr = np.array(zip(*randomRaDec))
-        if os.path.isfile(acpMask):
-            acpRegs = polyReadWkb(acpMask)
-            acpPrep = prep(acpRegs)
-            inside = map(lambda x, y: acpPrep.contains(Point(x, y)), raArr, decArr)
-        else:
-            inside = np.isfinite(raArr)
-        if os.path.isfile(rejMask):
-            rejRegs = polyReadWkb(rejMask)
-            rejPrep = prep(rejRegs)
-            masked = map(lambda x, y: rejPrep.contains(Point(x, y)), raArr, decArr)
-        else:
-            masked = np.isnan(raArr)
 
-        useful = map(lambda x, y: x and (not y), inside, masked)
-        randomUse = zip(raArr[useful], decArr[useful])
+            raArr, decArr = np.array(zip(*randomRaDec))
+            if os.path.isfile(acpMask):
+                acpRegs = polyReadWkb(acpMask)
+                acpPrep = prep(acpRegs)
+                inside = map(lambda x, y: acpPrep.contains(Point(x, y)),
+                             raArr, decArr)
+            else:
+                inside = np.isfinite(raArr)
+            if os.path.isfile(rejMask):
+                rejRegs = polyReadWkb(rejMask)
+                rejPrep = prep(rejRegs)
+                masked = map(lambda x, y: rejPrep.contains(Point(x, y)),
+                             raArr, decArr)
+            else:
+                masked = np.isnan(raArr)
+            useful = map(lambda x, y: x and (not y), inside, masked)
+            randomUse = zip(raArr[useful], decArr[useful])
+        except ImportError:
+            warnings.warn('Please install the Shapely')
+            randomUse = randomRaDec
     else:
         randomUse = randomRaDec
 
@@ -214,17 +220,17 @@ def makeRaDecCat(nRand, dataId=None, rangeRaDec=None, rad=None,
             galCat = astropy.table.Table.read(inputCat, format='fits')
             outCat = inputCat.strip().replace('.fits', '_radec.fits')
 
-            nGal   = len(galCat)
+            nGal = len(galCat)
 
             if nGal == nRand:
                 raArr, decArr = np.array(zip(*randomUse))
-                raCol  = astropy.table.Column(name='RA',  data=raArr)
+                raCol = astropy.table.Column(name='RA',  data=raArr)
                 decCol = astropy.table.Column(name='Dec', data=decArr)
                 galCat.add_columns([raCol, decCol])
             elif nGal < nRand:
                 import random
                 raArr, decArr = np.array(zip(*random.sample(randomUse, nGal)))
-                raCol  = astropy.table.Column(name='RA',  data=raArr)
+                raCol = astropy.table.Column(name='RA',  data=raArr)
                 decCol = astropy.table.Column(name='Dec', data=decArr)
                 galCat.add_columns([raCol, decCol])
             else:
@@ -232,15 +238,13 @@ def makeRaDecCat(nRand, dataId=None, rangeRaDec=None, rad=None,
                 indGal = np.arange(nGal)
                 galCatRand = galCat[random.sample(indGal, nRand)]
                 raArr, decArr = np.array(zip(*randomUse))
-                raCol  = astropy.table.Column(name='RA',  data=raArr)
+                raCol = astropy.table.Column(name='RA',  data=raArr)
                 decCol = astropy.table.Column(name='Dec', data=decArr)
                 galCatRand.add_columns([raCol, decCol])
                 galCat = galCatRand
 
             galCat.write(outCat, format='fits', overwrite=True)
-
         else:
             raise Exception('Can not find input catalog %s!' % inputCat)
 
     return randomUse
-
