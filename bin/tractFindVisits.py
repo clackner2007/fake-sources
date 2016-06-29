92bfef922639f07e8494cbbba01e11516140ba37
dr.guangtou <dr.guangtou@gmail.com>
2016-06-30 01:06:41 +0900

    Clean the code

--- bin/tractFindVisits.py
+++ bin/tractFindVisits.py
@@ -8,13 +8,10 @@ import numpy as np
 import lsst.daf.persistence as dafPersist
 from distutils.version import StrictVersion
 
+
 def tractFindVisits(rerun, tract, filter='HSC-I', patch=None,
-        dataDir='/lustre/Subaru/SSP/rerun/'):
-
-    """
-    Return the list of input Visits to coadd
-    """
-
+                    dataDir='/lustre/Subaru/SSP/rerun/'):
+    """Return the list of input Visits to coadd."""
     butler = dafPersist.Butler(os.path.join(dataDir, rerun))
 
     pipeVersion = dafPersist.eupsVersions.EupsVersions().versions['hscPipe']
@@ -24,28 +21,30 @@ def tractFindVisits(rerun, tract, filter='HSC-I', patch=None,
         coaddData = "deepCoadd"
 
     if patch is not '':
-
         """
         Only 1 Patch is required
         """
-        coadd = butler.get(coaddData, dataId = {"tract":tract,
-            "patch":patch, "filter":filter}, immediate=True)
+        coadd = butler.get(coaddData, dataId={"tract": tract,
+                                              "patch": patch,
+                                              "filter": filter},
+                           immediate=True)
         ccdInputs = coadd.getInfo().getCoaddInputs().ccds
         visits = np.unique(ccdInputs.get("visit"))
-        print "\n# Input visits for Tract=%d Filter=%s Patch=%s\n" % (tract, filter, patch)
-
+        print "\n# Visits for Tract=%d Filter=%s Patch=%s\n" % (tract,
+                                                                filter,
+                                                                patch)
     else:
-
         """
         Go through all the possible patches
         """
-
         visits = np.empty([0], dtype=int)
         for pa in itertools.combinations_with_replacement((np.arange(9)), 2):
             patch = str(pa[0]) + ',' + str(pa[1])
             try:
-                coadd = butler.get(coaddData, dataId = {"tract": tract,
-                    "patch": patch, "filter": filter}, immediate=True)
+                coadd = butler.get(coaddData, dataId={"tract": tract,
+                                                      "patch": patch,
+                                                      "filter": filter},
+                                   immediate=True)
             except Exception:
                 continue
             ccdInputs = coadd.getInfo().getCoaddInputs().ccds
@@ -55,8 +54,10 @@ def tractFindVisits(rerun, tract, filter='HSC-I', patch=None,
         for pa in itertools.combinations_with_replacement((np.arange(9)), 2):
             patch = str(pa[1]) + ',' + str(pa[0])
             try:
-                coadd = butler.get(coaddData, dataId = {"tract": tract,
-                    "patch": patch, "filter": filter}, immediate=True)
+                coadd = butler.get(coaddData, dataId={"tract": tract,
+                                                      "patch": patch,
+                                                      "filter": filter},
+                                   immediate=True)
             except Exception:
                 continue
             ccdInputs = coadd.getInfo().getCoaddInputs().ccds
@@ -80,14 +81,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("rerun", help="Name of the data rerun")
     parser.add_argument("tract", help="Tract number", type=int)
-    parser.add_argument('--dataDir', '-d', dest='dataDir', help="Data location",
-            default='/lustre/Subaru/SSP/rerun/')
-    parser.add_argument('--filter', '-f', dest='filter', help="Filter name",
-            default='HSC-I')
-    parser.add_argument('--patch', '-p', dest='patch', help="Patch ID",
-            default='')
+    parser.add_argument('--dataDir', '-d', dest='dataDir',
+                        help="Data location",
+                        default='/lustre/Subaru/SSP/rerun/')
+    parser.add_argument('--filter', '-f', dest='filter',
+                        help="Filter name",
+                        default='HSC-I')
+    parser.add_argument('--patch', '-p', dest='patch',
+                        help="Patch ID", default='')
 
     args = parser.parse_args()
 
     tractFindVisits(args.rerun, args.tract, filter=args.filter,
-            patch=args.patch, dataDir=args.dataDir)
+                    patch=args.patch, dataDir=args.dataDir)
