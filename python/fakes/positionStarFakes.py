1dbeb811b44dc2913915cbcfa950871d5fa84eba
dr.guangtou <dr.guangtou@gmail.com>
2016-06-30 01:48:59 +0900

    Clean the code

--- python/fakes/positionStarFakes.py
+++ python/fakes/positionStarFakes.py
@@ -1,20 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 import numpy as np
 
-import lsst.afw.image
-import lsst.afw.geom
-import lsst.afw.math
-import lsst.afw.cameraGeom
-import lsst.afw.coord
-import lsst.pex.config
-from lsst.pipe.tasks.fakes import FakeSourcesConfig, FakeSourcesTask
 import pyfits as fits
+
+import lsst.afw.geom as afwGeom
+import lsst.afw.math as afwMath
+import lsst.afw.coord as afwCoord
+import lsst.afw.image as afwImage
+import lsst.pex.config as afwConfig
+import lsst.afw.cameraGeom
+
+from lsst.pipe.tasks.fakes import FakeSourcesConfig, FakeSourcesTask
 
 import FakeSourceLib as fsl
 
+
 class PositionStarFakesConfig(FakeSourcesConfig):
-    starList = lsst.pex.config.Field(dtype=str, doc="catalog of stars with mags ra/dec to add")
-    seed = lsst.pex.config.Field(dtype=int, default=1,
-                                 doc="Seed for random number generator")
+    starList = afwConfig.Field(dtype=str,
+                               doc="Catalog of stars with mags ra/dec")
+    seed = afwConfig.Field(dtype=int, default=1,
+                           doc="Seed for random number generator")
+
 
 class PositionStarFakesTask(FakeSourcesTask):
     ConfigClass = PositionStarFakesConfig
@@ -22,22 +30,21 @@ class PositionStarFakesTask(FakeSourcesTask):
     def __init__(self, **kwargs):
         FakeSourcesTask.__init__(self, **kwargs)
         print "RNG seed:", self.config.seed
-        self.rng = lsst.afw.math.Random(self.config.seed)
+        self.rng = afwMath.Random(self.config.seed)
         self.npRand = np.random.RandomState(self.config.seed)
         self.starData = fits.open(self.config.starList)[1].data
-
 
     def run(self, exposure, background):
 
         self.log.info("Adding fake stars at real positions")
         psf = exposure.getPsf()
         psfBBox = psf.computeImage().getBBox()
-        margin =  max(psfBBox.getWidth(), psfBBox.getHeight())/2 + 1
+        margin = max(psfBBox.getWidth(), psfBBox.getHeight())/2 + 1
 
+        PARENT = afwImage.PARENT
         md = exposure.getMetadata()
-        expBBox = exposure.getBBox(lsst.afw.image.PARENT)
+        expBBox = exposure.getBBox(PARENT)
         wcs = exposure.getWcs()
-        skyToPixelMatrix=wcs.getLinearTransform().invert().getMatrix()/3600.0
 
         for istar, star in enumerate(self.starData):
             try:
@@ -48,44 +55,51 @@ class PositionStarFakesTask(FakeSourcesTask):
             try:
                 flux = exposure.getCalib().getFlux(float(star['mag']))
             except KeyError:
-                raise KeyError("No mag column in %s table"%self.config.starList)
+                raise KeyError("No mag column in %s" % self.config.starList)
 
             try:
-                starCoord = lsst.afw.coord.Coord(lsst.afw.geom.Point2D(star['RA'], star['DEC']))
+                starCoord = afwCoord.Coord(afwGeom.Point2D(star['RA'],
+                                                           star['DEC']))
             except KeyError:
-                raise("No RA/DEC column in %s table"%self.config.starList)
+                raise("No RA/DEC column in %s table" % self.config.starList)
 
             starXY = wcs.skyToPixel(starCoord)
-            bboxI = exposure.getBBox(lsst.afw.image.PARENT)
+            bboxI = exposure.getBBox(PARENT)
             bboxI.grow(margin)
-            if not bboxI.contains(lsst.afw.geom.Point2I(starXY)):
-                #self.log.info("Skipping fake %d"%starident)
+            if not bboxI.contains(afwGeom.Point2I(starXY)):
                 continue
 
             starImage = psf.computeImage(starXY)
             starImage *= flux
-            starBBox = starImage.getBBox(lsst.afw.image.PARENT)
+            starBBox = starImage.getBBox(PARENT)
 
-           #check that we're within the larger exposure, otherwise crop
+            # Check that we're within the larger exposure, otherwise crop
             if expBBox.contains(starBBox) is False:
-                newBBox = starImage.getBBox(lsst.afw.image.PARENT)
+                newBBox = starImage.getBBox(PARENT)
                 newBBox.clip(expBBox)
                 if newBBox.getArea() <= 0:
-                    self.log.info("Skipping fake %d"%starident)
+                    self.log.info("Skipping fake %d" % starident)
                     continue
-                self.log.info("Cropping FAKE%d from %s to %s"%(starident, str(starBBox), str(newBBox)))
-                starImage = starImage.Factory(starImage, newBBox, lsst.afw.image.PARENT)
+                self.log.info("Cropping FAKE%d from %s to %s" % (starident,
+                              str(starBBox), str(newBBox)))
+                starImage = starImage.Factory(starImage, newBBox, PARENT)
                 starBBox = newBBox
 
-            starMaskedImage = fsl.addNoise(starImage.convertF(), exposure.getDetector(),
+            starMaskedImage = fsl.addNoise(starImage.convertF(),
+                                           exposure.getDetector(),
                                            rand_gen=self.npRand)
 
             starMaskedImage.getMask().set(self.bitmask)
 
-            md.set("FAKE%s" % str(starident), "%.3f, %.3f" % (starXY.getX(), starXY.getY()))
-            self.log.info("Adding fake %s at: %.1f,%.1f"% (str(starident), starXY.getX(), starXY.getY()))
+            md.set("FAKE%s" % str(starident), "%.3f, %.3f" % (starXY.getX(),
+                                                              starXY.getY()))
+            self.log.info("Adding fake %s at: %.1f,%.1f" % (str(starident),
+                                                            starXY.getX(),
+                                                            starXY.getY()))
 
-            subMaskedImage = exposure.getMaskedImage().Factory(exposure.getMaskedImage(),
-                                                               starMaskedImage.getBBox(lsst.afw.image.PARENT),
-                                                               lsst.afw.image.PARENT)
+            maskedImage = exposure.getMaskedImage()
+            BBox = starMaskedImage.getBBox(PARENT)
+            subMaskedImage = maskedImage.Factory(exposure.getMaskedImage(),
+                                                 BBox,
+                                                 PARENT)
             subMaskedImage += starMaskedImage
